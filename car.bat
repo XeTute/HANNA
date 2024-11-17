a5b373d8db2c8e632a42404262c3c89e9a15870c
XeTute <x3tut3@gmail.com>
2024-11-17 10:33:46 -0500

    Add files via upload

---
+++ car.bat
@@ -0,0 +1,3 @@
+g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fopenmp -fwhole-program -fno-exceptions -fno-rtti -fexceptions main.cpp ANNA.hpp -o anna.exe
+anna.exe
+pause
