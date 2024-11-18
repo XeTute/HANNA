bcc7e428434452473462de3931e837075c5f93e8
XeTute <x3tut3@gmail.com>
2024-11-18 15:43:28 +0100

    Now only measures training time, added demo for ANNA::load

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <cmath>
 
 #include "ANNA.hpp"
 
@@ -10,21 +11,15 @@ using namespace XTTNNC;
 typedef time_point<high_resolution_clock> Timepoint;
 typedef high_resolution_clock hdc; // high res clock
 
+#define PATH "XOR.anna"
+
 int main()
 {
 	std::vector<counter> scale = { 2, 20, 10, 5, 1 }; // Doesn't have to be that large, but will 100% learn correctly.
-	ANNA<float> myNet(scale, 6);
-
-	Timepoint timepoint[2] = { hdc::now() };
+	ANNA<float> myNet;
+	myNet.setThreads(6);
 
 	std::cout << "Using " << omp_get_max_threads() << " threads.\n";
-
-	try
-	{
-		myNet.load("myNet.anna");
-		std::cout << "Loaded prev. ANNA model.\n";
-	}
-	catch (const std::exception& e) { std::cout << "Error: " << e.what() << "\nDidn't load model - Will train one from scratch.\n"; }
 	
 	myNet.lr = float(0.175);
 
@@ -35,29 +30,41 @@ int main()
 	};
 	counter md = data[0].size();
 
-	for (counter e = 0; e < 10000; ++e)
+	try
+	{
+		myNet.load(PATH);
+		std::cout << "Loaded model from " << PATH << ".\n";
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Error occurred while trying to load model from " << PATH << ": " << e.what() << ".\n";
+		myNet.init(scale);
+	}
+	
+
+	Timepoint timepoint[2] = { hdc::now() };
+	for (counter e = 0; e < 7000; ++e)
 	{
 		for (counter s = 0; s < md; ++s)
 		{
 			myNet.forward(data[0][s]);
 			myNet.backward(data[1][s]);
 		}
-
-		if (((e + 1) % 1000) == 0)
-		{
-			std::cout << "\n--- EPOCH " + std::to_string(e + 1) + " ---\n";
-			for (counter s = 0; s < md; ++s)
-			{
-				myNet.forward(data[0][s]);
-				std::cout << std::string(std::to_string(data[0][s][0]) + " | " + std::to_string(data[0][s][1]) + " || " + std::to_string(myNet.getOutput()[0])) << '\n';
-			}
-		}
 	}
-
-	myNet.save("myNet.anna");
-	std::cout << "Saved ANNA model.\n";
-
 	timepoint[1] = hdc::now();
-	std::cout << "Took " << duration_cast<milliseconds>(timepoint[1] - timepoint[0]).count() << "ms.\n";
+	std::cout << "Took " << duration_cast<milliseconds>(timepoint[1] - timepoint[0]).count() << "ms to train.\n";
+
+	float mse = 0.0f;
+	for (counter s = 0; s < md; ++s)
+	{
+		myNet.forward(data[0][s]);
+		float o = myNet.getOutput()[0];
+		mse = std::pow((data[1][s][0] - o), 2);
+		std::cout << std::string(std::to_string(data[0][s][0]) + " | " + std::to_string(data[0][s][1]) + " || " + std::to_string(o)) << '\n';
+	}
+	std::cout << "MSE: " << mse << '\n';
+
+	myNet.save(PATH);
+
 	return 0;
 }
