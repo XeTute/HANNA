011bcad788e36f336ab86b641ff3b75f10d2fc89
XeTute <x3tut3@gmail.com>
2024-11-27 16:47:35 +0100

    Removed -fomp

--- car.sh
+++ car.sh
@@ -1,3 +1,3 @@
-g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fopenmp -fwhole-program -fno-exceptions -fno-rtti -fexceptions main.cpp ANNA.hpp -o anna
+g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions main.cpp ANNA.hpp -o anna
 chmod +x anna
 ./anna
