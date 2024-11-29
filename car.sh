1bbc27290ff4d8d5751ed1806fdd3d30c8d5cc6d
XeTute <x3tut3@gmail.com>
2024-11-29 20:29:43 +0100

    c++17 => 14

--- car.sh
+++ car.sh
@@ -1,3 +1,3 @@
-g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions -pthread -std=c++17 main.cpp ANNA.hpp -o anna
+g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions -pthread main.cpp ANNA.hpp -o anna
 chmod +x anna
 ./anna
