bf6a26e1cfc99725c39ac2e40e1526529621fa48
XeTute <x3tut3@gmail.com>
2024-11-29 20:26:59 +0100

    README.md command for Windows

--- car.bat
+++ car.bat
@@ -1,3 +1,3 @@
-g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions main.cpp ANNA.hpp -o anna.exe
+g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions -mthread -std=c++17 main.cpp ANNA.hpp -o anna.exe
 anna.exe
 pause
