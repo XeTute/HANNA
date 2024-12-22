4321266be90e561ff83df118c8175a1330957385
XeTute <x3tut3@gmail.com>
2024-12-22 20:24:28 +0100

    Delete car.sh

--- car.sh
+++
@@ -1,3 +0,0 @@
-g++ -D__USE_MINGW_ANSI_STDIO=0 -O3 -Ofast -march=native -mtune=native -flto -fomit-frame-pointer -funroll-loops -ffast-math -fwhole-program -fno-exceptions -fno-rtti -fexceptions -pthread main.cpp ANNA-CPU.hpp -o anna
-chmod +x anna
-./anna
