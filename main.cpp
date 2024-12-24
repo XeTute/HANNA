110cf888b9b681b7def63ac3ff863dfecde7e917
XeTute <x3tut3@gmail.com>
2024-12-24 19:03:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ using namespace CSV;
 int main()
 {
     std::string header = "ID,Age";
-    std::vector<std::vector<float>> data =
+    std::vector<std::vector<unsigned short>> data =
     {
         { 0, 18 },
         { 1, 20 },
         { 2, 22 }
     };
 
-    if (saveCSV(data, header, "data.csv")) log("Success.", green, true);
+    if (saveCSVn(data, header, "data.csv")) log("Success.", green, true);
     else log("Fail.", red, true);
 
     return 0;
