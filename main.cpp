442996c512d3b0607b39453a32acfe12c81a382c
XeTute <x3tut3@gmail.com>
2024-12-28 18:45:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,59 +2,30 @@
 #include <chrono>
 
 #include "ANNA/ANNA-CPU/ANNA-CPU.hpp"
-
-using namespace ANNA_CPU;
-
-constexpr n EPOCHS = 10000;
+#include "ANNA/Data-Prep/CSV.hpp"
+#include "ANNA/Data-Prep/normalize.hpp"
 
 int main()
 {
-    ANNA model({2, 3, 1}, MATH::sigmoid, MATH::sigmoidDv);
-    
-    bool load = false;
-    std::cout << "> Load?: ";
-    std::cin >> load;
+    auto data(CSV::loadCSVn<float>("HousingData.csv"));
+    std::vector<float> data_flatten(0);
 
-    std::vector<std::vector<std::vector<float>>> data
+    n samples = data.size();
+    n eps = data[0].size(); // elements per sample
+
+    data_flatten.resize(samples * eps);
+    for (n i = 0; i < samples; ++i)
     {
-        { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } },
-        { { 0 }, { 1 }, { 1 }, { 0 } }
-    };
-    n SAMPLES = data[0].size();
-
-    if (load)
-    {
-        if (!model.load("model.ANNA", MATH::sigmoid)) std::cerr << "Loading model failed.\n";
-        else std::cout << "Loaded model.\n";
-    }
-    
-    else
-    {
-        model.lr = 0.1f;
-
-        std::cout << "Starting training...\n";
-        std::chrono::time_point<std::chrono::high_resolution_clock> tp[2] = { std::chrono::high_resolution_clock::now() };
-        for (n epoch = 0; epoch < EPOCHS; ++epoch)
-        {
-            for (n sample = 0; sample < SAMPLES; ++sample)
-            {
-                model.forwardForGrad(data[0][sample]);
-                model.gradDesc(data[1][sample]);
-            }
-        }
-        tp[1] = std::chrono::high_resolution_clock::now();
-
-        std::cout << "Training finished.\nTook " << std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count() << "ms\n>--- RESULTS ---<\n";
+        for (n elem = 0; elem < eps; ++elem)
+            data_flatten[i * eps + elem] = data[i][elem];
     }
 
-    for (n sample = 0; sample < SAMPLES; ++sample)
-    {
-        std::cout << "I: " << data[0][sample][0] << " : " << data[0][sample][1] << " => ";
-        model.forward(data[0][sample]);
-        auto o = model.getOutput();
-        for (float& x : o) std::cout << x;
-        std::cout << '\n'; 
-    }
+    normalize::normConf<float> data_config(normalize::minMaxNorm(data_flatten));
+
+    std::cout << "samples * eps = " << data_flatten.size() << ", data_config.x: " << data_config.x << ", data_config.y: " << data_config.y << '\n';
+    std::cout << "Example sample:\n";
+    for (n e = 0; e < eps; ++e)
+        std::cout << "E" << e << ": " << data_flatten[e] << '\n';
 
     return 0;
 }
