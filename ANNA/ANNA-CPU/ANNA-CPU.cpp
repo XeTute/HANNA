9baeea13e803255c4fbdaf0073e45e4c3f3e6e99
XeTute <x3tut3@gmail.com>
2025-01-11 20:16:30 +0100

    Multi-Threading, unoptimized though

--- ANNA/ANNA-CPU/ANNA-CPU.cpp
+++ ANNA/ANNA-CPU/ANNA-CPU.cpp
@@ -22,7 +22,6 @@ bool ANNA_CPU::ANNA::save(std::string path)
             }
         }
     }
-    saveWarning(false);
     w.close();
     return true;
 }
@@ -43,10 +42,10 @@ bool ANNA_CPU::ANNA::load(std::string path, void (*_activation)(float&))
         while (std::getline(ssBuffer, strBuffer, ';'))
             scale.push_back(std::stoull(strBuffer));
     }
+
     basicInit(scale, activation);
-    
-    std::vector<n> mlp_scale(scale);
-    mlp_scale.erase(mlp_scale.begin());
+    std::vector<n> mlp_scale(ds);
+
     for (n l = 0; l < dl; ++l)
     {
         std::vector<float> bias(mlp_scale[l], 0.f);
@@ -64,173 +63,28 @@ bool ANNA_CPU::ANNA::load(std::string path, void (*_activation)(float&))
                 return 0;
             }
         }
-        MLPL[l].pretrained(bias, weight_ll, true);
+        MLPL[l].pretrained(bias, weight_ll);
     }
     return true;
 }
 
-bool ANNA_CPU::ANNA::load(std::string path, void (*_activation)(float&), void (*_activationDV)(float&))
+void ANNA_CPU::ANNA::train(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& output, n epochs, void (*_activatioDV) (float&))
 {
-    std::ifstream r(path, std::ios::in | std::ios::binary);
-    if (!r.is_open() || !r) return false;
-    r.seekg(0);
-    scale = std::vector<n>(0);
-    {
-        std::string strBuffer("");
-        std::stringstream ssBuffer("");
-        char charBuffer = 0;
-        std::getline(r, strBuffer);
-        ssBuffer = std::stringstream(strBuffer);
-        
-        while (std::getline(ssBuffer, strBuffer, ';'))
-            scale.push_back(std::stoull(strBuffer));
-    }
-    basicInit(scale, activation);
-    enableTraining(_activationDV);
-
-    std::vector<n> mlp_scale(scale);
-    mlp_scale.erase(mlp_scale.begin());
-    for (n l = 0; l < dl; ++l)
-    {
-        std::vector<float> bias(mlp_scale[l], 0.f);
-        std::vector<std::vector<float>> weight_ll(mlp_scale[l], std::vector<float>(scale[l], 1.f));
-        if (!r.read(reinterpret_cast<char*>(bias.data()), bias.size() * sizeof(float)))
-        {
-            std::cerr << "Failed to read: std::ifstream returned false while trying to read binary. ANNA may be corrupted.\n";
-            return 0;
-        }
-        for (std::vector<float>& neuron : weight_ll)
-        {
-            if (!r.read(reinterpret_cast<char*>(neuron.data()), neuron.size() * sizeof(float)))
-            {
-                std::cerr << "Failed to read: std::ifstream returned false while trying to read binary. ANNA may be corrupted.\n";
-                return 0;
-            }
-        }
-        MLPL[l].pretrained(bias, weight_ll, true);
-    }
-    return true;
-}
-
-void ANNA_CPU::ANNA::train(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& output, n epochs)
-{
-    if (input.size() != output.size())
+    n samples = input.size();
+    if (samples != output.size())
     {
         std::cerr << "[ANNA-CPU train(...)]: The sizes of input & output do not match. Won't train.\n";
         return;
     }
 
-    n samples = input.size();
-    for (n i = 0; i < samples; ++i)
+    enableTraining(_activatioDV);
+    for (n e = 0; e < epochs; ++e)
     {
-        if (input[i].size() != scale[0])
+        for (n s = 0; s < samples; ++s)
         {
-            std::cout << "[ANNA-CPU train(...)]: The size of input[" << i << "] doesn't match with the network's input size. Won't train.\n";
-            return;
-        }
-        if (output[i].size() != scale[dl])
-        {
-            std::cout << "[ANNA-CPU train(...)]: The size of output[" << i << "] doesn't match with the network's output size. Won't train.\n";
-            return;
+            forwardForGrad(input[s]);
+            gradDesc(output[s]);
         }
     }
-
-    if (threads == 1)
-    {
-        for (n e = 0; e < epochs; ++e)
-        {
-            for (n s = 0; s < samples; ++s)
-            {
-                this->forwardForGrad(input[s]);
-                this->gradDesc(output[s]);
-            }
-        }
-        return;
-    }
-
-    n chunkSize = std::ceil(samples / threads);
-    n remainder = samples % threads;
-
-    std::vector<std::thread> pool(threads);
-    std::vector<ANNA> copy(threads, *this);
-
-    for (n thread = 0; thread < threads; ++thread)
-    {
-        pool[thread] = std::thread
-        (
-            [&](n id)
-            {
-                n end = id * chunkSize + chunkSize;
-                if ((threads - 1) == id) end += remainder;
-                for (n e = 0; e < epochs; ++e)
-                {
-                    for (n s = id * chunkSize; s < end; ++s)
-                    {
-                        copy[id].forwardForGrad(input[s]);
-                        copy[id].gradDesc(output[s]);
-                    }
-                }
-            }, thread
-        );
-    }
-
-    n iterations = (threads >= dl) ? dl : threads;
-
-    for (std::thread& thread : pool)
-        if (thread.joinable()) thread.join();
-
-    for (n l = 0; l < iterations; ++l)
-    {
-        pool[l] = std::thread
-        (
-            [&, l]
-            {
-                n neurons = MLPL[l].getBias().size();
-                n nll = MLPL[l].getWeights()[0].size();
-                float sum = 0.f;
-
-                for (n neuron = 0; neuron < neurons; ++neuron)
-                {
-                    sum = 0.f;
-                    for (ANNA& c : copy)
-                        sum += c.MLPL[l].getBias()[neuron];
-                    MLPL[l].getBias()[neuron] = sum / threads;
-
-                    for (n _nll = 0; _nll < nll; ++_nll)
-                    {
-                        sum = 0.f;
-                        for (ANNA& c : copy)
-                            sum += c.MLPL[l].getWeights()[neuron][_nll];
-                        MLPL[l].getWeights()[neuron][_nll] = sum / threads;
-                    }
-                }
-            }
-        );
-    }
-
-    for (n l = iterations; l < dl; l++)
-    {
-        n neurons = MLPL[l].getBias().size();
-        n nll = MLPL[l].getWeights()[0].size();
-        float sum = 0.f;
-
-        for (n neuron = 0; neuron < neurons; ++neuron)
-        {
-            sum = 0.f;
-            for (ANNA& c : copy)
-                sum += c.MLPL[l].getBias()[neuron];
-            MLPL[l].getBias()[neuron] = sum / threads;
-
-            for (n _nll = 0; _nll < nll; ++_nll)
-            {
-                sum = 0.f;
-                for (ANNA& c : copy)
-                    sum += c.MLPL[l].getWeights()[neuron][_nll];
-                MLPL[l].getWeights()[neuron][_nll] = sum / threads;
-            }
-        }
-    }
-
-    for (std::thread& thread : pool)
-        if (thread.joinable()) thread.join();
+    disableTraining();
 }
--- ANNA/ANNA-CPU/ANNA-CPU.hpp
+++ ANNA/ANNA-CPU/ANNA-CPU.hpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <thread>
 #include <iostream>
 #include <algorithm>
 
@@ -21,12 +20,11 @@ namespace ANNA_CPU
         std::vector<n> scale;
         std::vector<n> ds; // decreased scale, scale without the first element for more effective hidden & output layer construction
 
-        n layers;
-        n dl; // decreased layers
-        n ddl;
+        n layers, dl, ddl;
+        n threads;
 
-        unsigned short threads;
-        bool saved = false;
+        void (*activation) (float&);
+        void (*activationDV) (float&);
 
         void basicInit(std::vector<n> neurons, void (*_activation) (float&))
         {
@@ -39,108 +37,28 @@ namespace ANNA_CPU
 
             ds = neurons;
             ds.erase(ds.begin());
-            saved = false;
 
             MLPL.resize(dl);
-        }
-
-    public:
-
-        void (*activation)(float&);
-        void (*activationDV)(float&);
-
-        float lr = 0.1f;
-
-        ANNA() : MLPL(0), tmp_layer(0), scale(0), ds(0), layers(0), dl(0), ddl(0), threads(1), saved(false), activation(MATH::none), activationDV(MATH::none) {};
-        ANNA(std::vector<n> neurons, void (*_activation)(float&)) { this->init(neurons, _activation); }
-        ANNA(std::vector<n> neurons, void (*_activation)(float&), void (*_activationDV)(float&)) { this->init(neurons, _activation, _activationDV); }
-
-        void enableTraining(void (*_activationDV)(float&))
-        {
-            activationDV = _activationDV;
-            tmp_layer.resize(dl);
-
             for (n l = 0; l < dl; ++l)
             {
-                tmp_layer[l] = std::vector<float>(scale[l]);
-                MLPL[l].create(ds[l], scale[l], true);
+                MLPL[l].create(ds[l], scale[l]);
                 MLPL[l].rand();
             }
         }
-
-        void disableTraining()
-        {
-            activationDV = nullptr;
-            tmp_layer.resize(0);
-
-            for (n l = 0; l < dl; ++l)
-            {
-                MLPL[l].create(ds[l], scale[l], false);
-                MLPL[l].rand();
-            }
-        }
-
-        void init(std::vector<n> neurons, void (*_activation)(float&))
-        {
-            basicInit(neurons, _activation);
-            disableTraining();
-        }
-
-        void init(std::vector<n> neurons, void (*_activation)(float&), void (*_activationDV)(float&))
-        {
-            basicInit(neurons, _activation);
-            enableTraining(_activationDV);
-        }
-
-        n getNParameters()
-        {
-            n params = scale[0];
-            for (n i = 1; i < dl; ++i) params += scale[i] + scale[i] * scale[i - 1];
-            return params;
-        }
-
-        bool save(std::string path);
-        void saveWarning(bool _save) { saved = !_save; }
-        bool load(std::string path, void (*_activation)(float&));
-        bool load(std::string path, void (*_activation)(float&), void (*_activationDV)(float&));
-
-        void display()
-        {
-            std::cout << "\n\nDISPLAY START";
-            for (n l = 0; l < dl; ++l)
-            {
-                std::cout << "\n<--- LAYER " << l << " --->\n";
-                std::cout << "Neuron-Count: " << MLPL[l].getState().size() << '\n';
-                std::cout << "Bias: \n";
-                for (float bias : MLPL[l].getBias()) std::cout << "- " << bias << '\n';
-
-                std::cout << "\nWeights: \n";
-                for (n neuron = 0; neuron < MLPL[l].getBias().size(); ++neuron)
-                {
-                    std::cout << ">-- NEURON " << neuron << " <--\n";
-                    for (n nll = 0; nll < MLPL[l].getWeights()[0].size(); ++nll)
-                        std::cout << "- " << MLPL[l].getWeights()[neuron][nll] << '\n';
-                }
-            }
-            std::cout << "DISPLAY END\n\n";
-        }
-
-        void setThreads(unsigned short _threads) { threads = _threads; }
-        unsigned short getThreads() { return threads; }
 
         void forward(const std::vector<float>& inp)
         {
-            MLPL[0].forward(inp, activation);
+            MLPL[0].forward(inp, activation, threads);
             for (n l = 1; l < dl; ++l)
-                MLPL[l].forward(MLPL[l - 1].getState(), activation);
+                MLPL[l].forward(MLPL[l - 1].getState(), activation, threads);
         }
 
         void forwardForGrad(const std::vector<float>& inp)
         {
             tmp_layer[0] = inp;
-            MLPL[0].forward(inp, activation);
+            MLPL[0].forward(inp, activation, threads);
             for (n l = 1; l < dl; ++l)
-                MLPL[l].forward(MLPL[l - 1].getState(), activation);
+                MLPL[l].forward(MLPL[l - 1].getState(), activation, threads);
         }
 
         void gradDesc(const std::vector<float>& expected_output)
@@ -151,10 +69,65 @@ namespace ANNA_CPU
             MLPL[0].gradDesc(tmp_layer[1], tmp_layer[0], activationDV, lr);
         }
 
+        void enableTraining(void (*_activationDV) (float&))
+        {
+            activationDV = _activationDV;
+            tmp_layer.resize(dl);
+
+            for (n l = 0; l < dl; ++l)
+            {
+                tmp_layer[l] = std::vector<float>(scale[l]);
+                MLPL[l].enableTraining();
+            }
+        }
+
+        void disableTraining()
+        {
+            activationDV = nullptr;
+            tmp_layer.resize(0);
+
+            for (auto& l : MLPL) l.disableTraining();
+        }
+    public:
+
+        float lr = 0.1f;
+
+        ANNA() : MLPL(0), tmp_layer(0), scale(0), ds(0), layers(0), dl(0), ddl(0), threads(1), activation(MATH::none), activationDV(MATH::none) {};
+        ANNA(std::vector<n> neurons, void (*_activation) (float&)) { init(neurons, _activation); }
+
+        std::vector<float> fw(std::vector<float>& inp, bool softmax)
+        {
+            forward(inp);
+            if (softmax) calcSoftmaxOut();
+            return getOutput();
+        }
+
+        void init(std::vector<n> neurons, void (*_activation) (float&))
+        {
+            basicInit(neurons, _activation);
+            disableTraining();
+        }
+        
+        void setThreads(n _threads)
+        {
+            threads = _threads;
+            for (MLP::LAYER& l : MLPL) l.setThreads(threads);
+        }
+
+        n getNParameters()
+        {
+            n params = scale[0];
+            for (n i = 1; i < dl; ++i) params += scale[i] + scale[i] * scale[i - 1];
+            return params;
+        }
+
+        bool save(std::string path);
+        bool load(std::string path, void (*_activation) (float&));
+
         const std::vector<float>& getOutput() { return MLPL[ddl].getState(); }
         void calcSoftmaxOut() { MLPL[ddl].softmax(); }
 
-        void train(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& output, n epochs);
+        void train(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& output, n epochs, void (*_activationDV) (float&));
 
         void operator=(const ANNA& other)
         {
@@ -170,31 +143,11 @@ namespace ANNA_CPU
             this->threads = other.threads;
             this->activation = other.activation;
             this->activationDV = other.activationDV;
-            this->saved = other.saved;
             this->lr = other.lr;
         }
 
-        void destruct()
+        ~ANNA()
         {
-            if (!saved)
-            {
-                std::cerr << "[ANNA-CPU ~ANNA()]: The current ANNA model hasn't been saved yet.\n"
-                    "[ANNA-CPU ~ANNA()]: This warning can be turned of using `ANNA::saveWarning(false);`\n"
-                    "[ANNA-CPU ~ANNA()]: Do you wish to save the model? (0: No, 1: Yes): ";
-                bool choice = true;
-                std::cin >> choice;
-
-                if (choice)
-                {
-                    std::string filename = std::to_string(reinterpret_cast<std::uintptr_t>(this)) + std::string(".ANNA");
-                    std::cout << "Trying to save under " << filename << "...\n";
-                    if (this->save(filename)) std::cout << "Success.\n";
-                    else std::cerr << "Failed.\n";
-                }
-                else std::cout << "[ANNA-CPU ~ANNA()]: Didn't save the model as per choice.\n";
-                saveWarning(false);
-            }
-
             MLPL.clear();
             tmp_layer.clear();
             scale.clear();
@@ -208,10 +161,8 @@ namespace ANNA_CPU
             activation = nullptr;
             activationDV = nullptr;
 
-            lr = 0.1;
+            lr = 0.1f;
         }
-
-        ~ANNA() {}
     };
 }
 
