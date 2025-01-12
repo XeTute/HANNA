dd715b7c7f0d8bef04d2dd53eb49559a70f7eb21
XeTute <x3tut3@gmail.com>
2025-01-12 17:34:40 +0100

    Delete build directory

--- build/c.bat
+++
@@ -1,3 +0,0 @@
-echo off
-g++ main.cpp "ANNA/ANNA-CPU/ANNA-CPU.hpp" "ANNA/ANNA-CPU/ANNA-CPU.cpp" "ANNA/Data-Prep/CSV.hpp" -O3 -o "build/main.exe"
-echo on
--- build/car.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ main.cpp "ANNA/ANNA-CPU/ANNA-CPU.hpp" "ANNA/ANNA-CPU/ANNA-CPU.cpp" "ANNA/Data-Prep/CSV.hpp" "ANNA/ANNA-CPU/Math-Fun.hpp" "ANNA/ANNA-CPU/MLP-Layer.hpp" -O3 -o "build/main.exe"
-build\main.exe
-@echo on
