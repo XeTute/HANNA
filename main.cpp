d7b9be4dc4b528ef9c41614a0445976074a509d9
XeTute <x3tut3@gmail.com>
2025-01-14 22:50:11 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@
 // Test-Code licensed under MIT, HANNA Library under Apache2.0, and multi for Eigen
 // Port to OpenCL expected in four - x months.
 
-float activation(float x) { return x; }
+void activation(float& x) { x = x; }
 int main()
 {
 	PERCEPTRON::LAYER model;
 	if (model.birth(6, 1)) std::cout << "Success\n";
 	else std::cout << "Fail\n";
 	
+	wb::effarr<float> input(6, 2.f);
+	input = model.inference(input, activation);
+
+	std::cout << input[0] << '\n';
+
 	model.suicide();
 	return 0;
 }
