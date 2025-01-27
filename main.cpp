80f08d2cd559898b0b4596c9aa169292cc529929
XeTute Technologies <x3tut3@gmail.com>
2025-01-27 18:47:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 void sigmoid(float& x) { x = 1.f / (1.f + std::exp(-x)); }
 void sigmoidDV(float& x) { x *= (1.f - x); }
 
-struct normConf { float min, delta; };
+struct normConf { float min, delta; }; // delta = max - min
 void minmaxnorm(std::vector<float>& a, normConf conf)
 {
 	if (a.empty()) return;
@@ -55,7 +55,61 @@ int main()
 	omp_set_num_threads(6);
 
 	MLP::MLP hdm; // heart disease model
-	hdm.birth({ });
+	hdm.birth({ 12, 7, 7, 3, 1 });
+
+	auto data = readCSV("cardio-hdd.csv");
+
+	std::size_t rows = data.size();
+	std::size_t cols = data[0].size();
+	std::size_t inputs = cols - 1;
+
+	std::vector<std::vector<float>> input(rows, std::vector<float>(inputs));
+	std::vector<std::vector<float>> output(rows, std::vector<float>(1));
+
+	std::vector<normConf> conf(inputs, { 0.f, 0.f });
+
+	for (std::size_t col = 0; col < inputs; ++col)
+	{
+		std::vector<float> coldata(rows);
+#pragma omp parallel for
+		for (long long int row = 0; row < rows; ++row)
+			coldata[row] = data[row][col];
+
+		float min = *std::min_element(coldata.begin(), coldata.end());
+		conf[col] = { min, *std::max_element(coldata.begin(), coldata.end()) - min };
+		minmaxnorm(coldata, conf[col]);
+
+#pragma omp parallel for
+		for (long long int row = 0; row < rows; ++row)
+			data[row][col] = coldata[row];
+	}
+	std::cout << "Normalized data.\n";
+
+	for (std::size_t row = 0; row < rows; ++row)
+	{
+		for (std::size_t col = 0; col < inputs; ++col)
+			input[row][col] = data[row][col];
+		output[row][0] = data[row][inputs];
+	}
+
+	data.clear();
+	std::cout << "Formatted data.\n";
+
+	float lr = 0.05f;
+	std::size_t epochs = 100;
+	std::chrono::high_resolution_clock::time_point tp[2];
+
+	hdm.enableTraining();
+	tp[0] = std::chrono::high_resolution_clock::now();
+	hdm.train(input, output, sigmoid, sigmoidDV, lr, epochs);
+	tp[1] = std::chrono::high_resolution_clock::now();
+	hdm.disableTraining();
+
+	std::cout << "Took " << std::chrono::duration_cast<std::chrono::seconds>(tp[1] - tp[0]).count() << "s.\n";
+
+	hdm.save("HDM.MLP");
+	hdm.forward(input[0], sigmoid);
+	std::cout << "Output: " << hdm.out()[0] << '.' << std::endl;
 
 	return 0;
 }
