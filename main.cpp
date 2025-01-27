073478d060d5f3dc839baec12129f5ddfbf08b96
XeTute Technologies <x3tut3@gmail.com>
2025-01-27 14:57:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,61 @@
 #include <iostream>
+#include <sstream>
 #include <chrono>
 #include <cmath>
+
 #include "HANNA/HANNA.hpp"
 
-// Test 0.01 for HANNA
-// Test trains a MLP model on predicting / generating shakespeare-like text, after training it
-// Not Suitable for production usages. Consider using small Transformer, RNN or CNN models for that.
-// Test-Code licensed under MIT, HANNA Library under Apache2.0.
-// Port to OpenCL expected in four - x months.
-
 void sigmoid(float& x) { x = 1.f / (1.f + std::exp(-x)); }
-void sigmoidDV(float& x) { x *= (1 - x); }
+void sigmoidDV(float& x) { x *= (1.f - x); }
 
-constexpr std::size_t ctx = 12;
+struct normConf { float min, delta; };
+void minmaxnorm(std::vector<float>& a, normConf conf)
+{
+	if (a.empty()) return;
+	if (conf.min == *std::max_element(a.begin(), a.end())) std::fill(a.begin(), a.end(), 0.f);
+
+	long long int size = a.size();
+#pragma omp parallel for
+	for (long long int i = 0; i < size; ++i)
+		a[i] = (a[i] - conf.min) / conf.delta;
+}
+
+std::vector<std::vector<float>> readCSV(std::string path)
+{
+	std::ifstream r(path, std::ios::in);
+	if (!r || !r.is_open() || !r.good()) return std::vector<std::vector<float>>(0);
+
+	std::vector<std::vector<float>> data(0);
+	std::size_t elems = 0;
+
+	std::string buffer("");
+	std::string elem("");
+
+	std::getline(r, buffer); // First line is header
+	{
+		std::stringstream header(buffer);
+		while (std::getline(header, elem, ','))
+			++elems;
+	}
+
+	while (std::getline(r, buffer))
+	{
+		std::stringstream row(buffer);
+		std::vector<float> rowvec(elems);
+		
+		for (std::size_t i = 0; std::getline(row, elem, ','); ++i)
+			rowvec[i] = std::stof(elem);
+		data.push_back(rowvec);
+	}
+	return data;
+}
 
 int main()
 {
-	std::chrono::high_resolution_clock::time_point tp[2]; // Benchmarking
+	omp_set_num_threads(6);
 
-	// Parse data from file
-	tp[0] = std::chrono::high_resolution_clock::now();
-	std::string strdata("");
-	{
-		std::ifstream r("tiny-shakespeare.txt", std::ios::in);
-		std::string buf;
-		while (std::getline(r, buf))
-			strdata += buf + '\n';
-	}
-	std::size_t samples = strdata.size() - ctx;
-
-	std::vector<std::vector<float>> inp(samples, std::vector<float>(ctx , 0.f));
-	std::vector<std::vector<float>> out(samples, std::vector<float>(255, 0.f));
-
-	for (std::size_t s = 0; s < samples; ++s)
-	{
-		for (std::size_t c = 0; c < ctx; ++c)
-			inp[s][c] = (float)strdata[s + c];
-		out[s][(int)strdata[s + ctx + 1]] = 1.f;
-	}
-
-	tp[1] = std::chrono::high_resolution_clock::now();
-	std::cout << "Parsed the data in " << std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count() << "ms.\n";
-
-	// Define the model
-	tp[0] = std::chrono::high_resolution_clock::now();
-
-	MLP::MLP vs( { ctx, (ctx + 255) / 2, 255} ); // vs virtual shakespeare
-	float lr = 0.05f;
-	
-	tp[1] = std::chrono::high_resolution_clock::now();
-	std::cout << "Defined the model in " << std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count() << "ms.\n";
-
-	// Train the model
-	tp[0] = std::chrono::high_resolution_clock::now();
-
-	omp_set_num_threads(6); // Speeds training up a little using OMP
-	vs.enableTraining(); // Enable training, isn't enabled by default to save some memory
-	vs.train(inp, out, sigmoid, sigmoidDV, lr, 2); // Train the model on the data parsed using Sigmoid and two epochs
-	vs.disableTraining(); // Disable training after training to save some memory
-
-	tp[1] = std::chrono::high_resolution_clock::now();
-	std::cout << "Trained the model in " << std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count() << "ms.\n";
-
-	// Save the model
-	if (vs.save("VirtualShakespeare.MLP")) std::cout << "Saved Model!\n";
-	else std::cout << "Failed to save Model =(\n";
-
-	// Inference it
-	std::string input("");
-	std::vector<float> inpvec(ctx, 0.f);
-	while (input != std::string("---EXIT---"))
-	{
-		std::cout << "> ";
-		std::getline(std::cin, input);
-
-		if (input.size() > ctx) input = input.substr(0, 12);
-		for (unsigned short c = 0; c < input.size(); ++c)
-			inpvec[c] = (float)input[c];
-
-		auto out = vs.out(inpvec, sigmoid);
-		std::cout << "< " << char(std::max_element(out.begin(), out.end()) - out.begin()) << '\n';
-
-		inpvec = std::vector<float>(ctx, 0.f);
-	}
+	MLP::MLP hdm; // heart disease model
+	hdm.birth({ });
 
 	return 0;
 }
